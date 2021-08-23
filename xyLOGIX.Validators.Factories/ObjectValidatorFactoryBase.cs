b0dd393ce537c678280810cc79b5bb700a2b11e1
Brian Hart <brian.hart@xylogix.net>
2021-08-23 13:24:35 -0500

    Create abstract object-validator factory

---
+++ xyLOGIX.Validators.Factories/ObjectValidatorFactoryBase.cs
@@ -0,0 +1,30 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using xyLOGIX.Validators.Interfaces;
+
+namespace xyLOGIX.Validators.Factories
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:xyLOGIX.Validators.Interfaces.IObjectValidator" /> interface, and
+    /// returns references to them.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    public abstract class ObjectValidatorFactoryBase<TObject>
+        where TObject : class
+    {
+        /*
+         * TODO: Derive a child class from this one that is a Singleton for the desired object type.  Name the Singleton instance property 'For' for semantic consistency.
+         */
+
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IObjectValidator" /> interface and
+        /// returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IObjectValidator" /> interface.
+        /// </returns>
+        public abstract IObjectValidator Object(TObject objectToValidate);
+    }
+}
