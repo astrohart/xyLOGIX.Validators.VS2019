f3b37307e1ecda3607c33dc43fa29371c4e741e7
Brian Hart <brian.hart@xylogix.net>
2021-08-23 16:50:10 -0500

    Create ValidationSucceeded event

---
+++ xyLOGIX.Validators.Events/ValidationSucceededEventArgs.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace xyLOGIX.Validators.Events
+{
+    /// <summary>
+    /// Provides information for ValidationSucceeded event handlers.
+    /// </summary>
+    public class ValidationSucceededEventArgs : EventArgs { }
+}
---
+++ xyLOGIX.Validators.Events/ValidationSucceededEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace xyLOGIX.Validators.Events
+{
+    /// <summary>
+    /// Represents a handler for a ValidationSucceeded event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see cref="T:xyLOGIX.Validators.Events.ValidationSucceededEventArgs"/> that contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the ValidationSucceeded event.
+    /// </remarks>
+    public delegate void ValidationSucceededEventHandler(object sender,
+        ValidationSucceededEventArgs e);
+}
