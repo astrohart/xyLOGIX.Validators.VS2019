e8753dc955ec9140e0992165f4be01c2148f39aa
Brian Hart <brian.hart@xylogix.net>
2021-08-23 16:47:08 -0500

    Create ValidationFailed event args and delegate

---
+++ xyLOGIX.Validators.Events/ValidationFailedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace xyLOGIX.Validators.Events
+{
+    /// <summary>
+    /// Provides information for ValidationFailed event handlers.
+    /// </summary>
+    public class ValidationFailedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructs a new instance of <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs"/> and returns a reference to it.
+        /// </summary>
+        public ValidationFailedEventArgs()
+        {
+            // TODO: Add property initializers here
+        }
+    }
+}
---
+++ xyLOGIX.Validators.Events/ValidationFailedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace xyLOGIX.Validators.Events
+{
+    /// <summary>
+    /// Represents a handler for a ValidationFailed event.
+    /// </summary>
+    /// <param name="sender">
+    /// Reference to the instance of the object that raised the event.
+    /// </param>
+    /// <param name="e">
+    /// A <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs"/> that contains the event data.
+    /// </param>
+    /// <remarks>
+    /// This delegate merely specifies the signature of all methods that handle the ValidationFailed event.
+    /// </remarks>
+    public delegate void ValidationFailedEventHandler(object sender,
+        ValidationFailedEventArgs e);
+}
