0714ebec30d09a011b89addc3436c75abe02afc0
Brian Hart <brian.hart@xylogix.net>
2021-08-23 16:48:42 -0500

    Update docs, add error message property

--- xyLOGIX.Validators.Events/ValidationFailedEventArgs.cs
+++ xyLOGIX.Validators.Events/ValidationFailedEventArgs.cs
@@ -5,14 +5,26 @@ namespace xyLOGIX.Validators.Events
     /// <summary>
     /// Provides information for ValidationFailed event handlers.
     /// </summary>
+    /// <remarks>
+    /// Such events normally are raised when we failed to validate an object's
+    /// properties' values according to a given set of rules.
+    /// </remarks>
     public class ValidationFailedEventArgs : EventArgs
     {
         /// <summary>
-        /// Constructs a new instance of <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs"/> and returns a reference to it.
+        /// Constructs a new instance of
+        /// <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs" /> and
+        /// returns a reference to it.
         /// </summary>
-        public ValidationFailedEventArgs()
-        {
-            // TODO: Add property initializers here
-        }
+        /// <param name="errorMessage">
+        /// (Required.) String containing the error message to be generated.
+        /// </param>
+        public ValidationFailedEventArgs(string errorMessage)
+            => ErrorMessage = errorMessage;
+
+        /// <summary>
+        /// Gets a string explaining why the validation failed.
+        /// </summary>
+        public string ErrorMessage { get; }
     }
 }
