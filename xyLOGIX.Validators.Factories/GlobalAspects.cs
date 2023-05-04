e2e67a2d677a5cabf0664285b2c131c90c0aa32b
Brian Hart <brian.hart@xylogix.net>
2023-05-04 09:08:43 -0600

    Latest updates

--- xyLOGIX.Validators.Factories/GlobalAspects.cs
+++ xyLOGIX.Validators.Factories/GlobalAspects.cs
@@ -2,6 +2,21 @@ using PostSharp.Extensibility;
 using PostSharp.Patterns.Diagnostics;
 
 // This file contains registration of aspects that are applied to several classes of this project.
-[assembly: Log(AttributeTargetTypeAttributes = MulticastAttributes.Private | MulticastAttributes.Protected | MulticastAttributes.Internal | MulticastAttributes.Public, AttributeTargetMemberAttributes = MulticastAttributes.Private | MulticastAttributes.Protected | MulticastAttributes.Internal | MulticastAttributes.Public | MulticastAttributes.UserGenerated)]
+[assembly:
+    Log(
+        AttributeTargetTypeAttributes = MulticastAttributes.Private |
+                                        MulticastAttributes.Protected |
+                                        MulticastAttributes.Internal |
+                                        MulticastAttributes.Public,
+        AttributeTargetMemberAttributes = MulticastAttributes.Private |
+                                          MulticastAttributes.Protected |
+                                          MulticastAttributes.Internal |
+                                          MulticastAttributes.Public |
+                                          MulticastAttributes.UserGenerated
+    )]
 [assembly: Log(AttributePriority = 1)]
-[assembly: Log(AttributeExclude = true, AttributeTargetMembers = "regex:^get_|^set_", AttributePriority = 2)]
+[assembly:
+    Log(
+        AttributeExclude = true, AttributeTargetMembers = "regex:^get_|^set_|^add_|^remove_",
+        AttributePriority = 2
+    )]
