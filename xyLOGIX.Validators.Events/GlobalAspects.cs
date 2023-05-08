286508c701073cc2350f422a9cbc27d663a2493d
Brian Hart <brian.hart@xylogix.net>
2023-05-08 15:47:09 -0600

    Update GlobalAspects.cs, other changes

--- xyLOGIX.Validators.Events/GlobalAspects.cs
+++ xyLOGIX.Validators.Events/GlobalAspects.cs
@@ -15,8 +15,7 @@ using PostSharp.Patterns.Diagnostics;
                                           MulticastAttributes.UserGenerated
     )]
 [assembly: Log(AttributePriority = 1)]
-[assembly:
-    Log(
-        AttributeExclude = true, AttributeTargetMembers = "regex:^get_|^set_|^add_|^remove_",
-        AttributePriority = 2
-    )]
+[assembly: Log(AttributePriority = 2, AttributeExclude = true, AttributeTargetMembers = "get_*" )]
+[assembly: Log(AttributePriority = 3, AttributeExclude = true, AttributeTargetMembers = "set_*" )]
+[assembly: Log(AttributePriority = 4, AttributeExclude = true, AttributeTargetMembers = "add_*" )]
+[assembly: Log(AttributePriority = 5, AttributeExclude = true, AttributeTargetMembers = "remove_*" )]
