f42a18eb7bd91e1da7f29edfabeef7f6cf893099
Brian Hart <brian.hart@xylogix.net>
2024-01-23 06:33:48 -0700

    Move IDataValidator.cs to xyLOGIX.Validators module

---
+++ xyLOGIX.Validators.Interfaces/IDataValidator.cs
@@ -0,0 +1,14 @@
+﻿using PostSharp.Patterns.Threading;
+
+namespace xyLOGIX.Validators.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties that are in common to all
+    /// data validators.
+    /// </summary>
+    [Actor]
+    public interface IDataValidator
+    {
+        // TODO: Add events, methods, and properties to your interface here.
+    }
+}
