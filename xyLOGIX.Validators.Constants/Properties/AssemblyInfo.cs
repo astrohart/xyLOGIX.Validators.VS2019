cb271de4dc905b33e7e36303299c3635011833c3
Brian Hart <brian.hart@xylogix.net>
2024-01-24 10:52:58 -0700

    Create xyLOGIX.Validators.Constants.csproj

---
+++ xyLOGIX.Validators.Constants/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX.Validators.Constants")]
+[assembly: AssemblyDescription("Provides objects and functionality for ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for xyLOGIX.Validators.Constants")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2020-23 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Foo.  Foo is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f416f56-3b77-4efc-a12f-26a1b405b4aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ xyLOGIX.Validators.Constants/README.md
@@ -0,0 +1,34 @@
+<a name='assembly'></a>
+# xyLOGIX.Validators.Constants
+
+## Contents
+
+- [Resources](#T-xyLOGIX.Validators.Constants-Properties-Resources 'xyLOGIX.Validators.Constants.Properties.Resources')
+  - [Culture](#P-xyLOGIX.Validators.Constants-Properties-Resources-Culture 'xyLOGIX.Validators.Constants.Properties.Resources.Culture')
+  - [ResourceManager](#P-xyLOGIX.Validators.Constants-Properties-Resources-ResourceManager 'xyLOGIX.Validators.Constants.Properties.Resources.ResourceManager')
+
+<a name='T-xyLOGIX.Validators.Constants-Properties-Resources'></a>
+## Resources `type`
+
+##### Namespace
+
+xyLOGIX.Validators.Constants.Properties
+
+##### Summary
+
+A strongly-typed resource class, for looking up localized strings, etc.
+
+<a name='P-xyLOGIX.Validators.Constants-Properties-Resources-Culture'></a>
+### Culture `property`
+
+##### Summary
+
+Overrides the current thread's CurrentUICulture property for all
+  resource lookups using this strongly typed resource class.
+
+<a name='P-xyLOGIX.Validators.Constants-Properties-Resources-ResourceManager'></a>
+### ResourceManager `property`
+
+##### Summary
+
+Returns the cached ResourceManager instance used by this class.
