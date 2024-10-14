c18a6e5da84ebfa5df8279fd5c8b1d0d4bb03a3e
Brian Hart <brian.hart@xylogix.net>
2024-10-13 18:36:17 -0600

    Latest updates at 18:36 hours MDT on 10/13/2024

--- xyLOGIX.Validators.Interfaces/ITCPPortValidator.cs
+++ xyLOGIX.Validators.Interfaces/ITCPPortValidator.cs
@@ -1,8 +1,39 @@
 namespace xyLOGIX.Validators.Interfaces
 {
     /// <summary>
-    /// TODO: Add interface documentation here.
+    /// Defines the publicly-exposed events, methods and properties of a TCP Port
+    /// Number validator.
     /// </summary>
     public interface ITCPPortValidator
-    { }
+    {
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.String" />, that
+        /// represents the TCP <paramref name="port" /> number using ASCII characters, is
+        /// (a) parsable as an integer and (b) is in the range of valid values.
+        /// </summary>
+        /// <param name="port">
+        /// (Required.) A <see cref="T:System.String" /> that contains
+        /// the ASCII representation of a TCP port number.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="port" /> is
+        /// parsable as an integer, and in the range <c>1</c>-<c>65535</c> (inclusive);
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        bool IsValid(string port);
+
+        /// <summary>
+        /// Determines whether the specified TCP <paramref name="port" /> number is in the
+        /// range of valid values.
+        /// </summary>
+        /// <param name="port">
+        /// (Required.) An <see cref="T:System.Int32" /> that set to the TCP port number
+        /// that is to be validated.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="port" /> is in the
+        /// range <c>1</c>-<c>65535</c> (inclusive); otherwise, <see langword="false" />.
+        /// </returns>
+        bool IsValid(int port);
+    }
 }
--- xyLOGIX.Validators/TCPPortValidator.cs
+++ xyLOGIX.Validators/TCPPortValidator.cs
@@ -1,12 +1,15 @@
 using PostSharp.Patterns.Diagnostics;
+using System;
 using System.Diagnostics;
-
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Core.Extensions;
 using xyLOGIX.Validators.Interfaces;
 
 namespace xyLOGIX.Validators
 {
     /// <summary>
-    /// TODO: Add class documentation here.
+    /// Exposes functionality to validate the values of variables that purport to hold
+    /// TCP port numbers.
     /// </summary>
     public class TCPPortValidator : ITCPPortValidator
     {
@@ -20,14 +23,97 @@ namespace xyLOGIX.Validators
         /// Empty, protected constructor to prohibit direct allocation of this class.
         /// </summary>
         [Log(AttributeExclude = true)]
-        protected TCPPortValidator()
-        { }
+        protected TCPPortValidator() { }
 
         /// <summary>
         /// Gets a reference to the one and only instance of the object that implements the
         /// <see cref="T:xyLOGIX.Validators.Interfaces.ITCPPortValidator" /> interface.
         /// </summary>
-        public static ITCPPortValidator Instance { [DebuggerStepThrough] get; } =
-            new TCPPortValidator();
+        public static ITCPPortValidator
+            Instance { [DebuggerStepThrough] get; } = new TCPPortValidator();
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.String" />, that
+        /// represents the TCP <paramref name="port" /> number using ASCII characters, is
+        /// (a) parsable as an integer and (b) is in the range of valid values.
+        /// </summary>
+        /// <param name="port">
+        /// (Required.) A <see cref="T:System.String" /> that contains
+        /// the ASCII representation of a TCP port number.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="port" /> is
+        /// parsable as an integer, and in the range <c>1</c>-<c>65535</c> (inclusive);
+        /// otherwise, <see langword="false" />.
+        /// </returns>
+        public bool IsValid(string port)
+        {
+            var result = false;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(port)) return result;
+                if (!int.TryParse(port, out var portNumber)) return result;
+
+                result = PortNumberIsInCorrectRange(portNumber);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether the specified TCP <paramref name="port" /> number is in the
+        /// range of valid values.
+        /// </summary>
+        /// <param name="port">
+        /// (Required.) An <see cref="T:System.Int32" /> that set to the TCP port number
+        /// that is to be validated.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="port" /> is in the
+        /// range <c>1</c>-<c>65535</c> (inclusive); otherwise, <see langword="false" />.
+        /// </returns>
+        public bool IsValid(int port)
+        {
+            bool result;
+
+            try
+            {
+                result = PortNumberIsInCorrectRange(port);
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = false;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <paramref name="portNumber" /> is within the
+        /// defined range of valid values.
+        /// </summary>
+        /// <param name="portNumber">
+        /// (Required.) An <see cref="T:System.Int32" /> that set to the TCP port number
+        /// that is to be validated.
+        /// </param>
+        /// <returns></returns>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="portNumber" /> is in
+        /// the
+        /// range <c>1</c>-<c>65535</c> (inclusive); otherwise, <see langword="false" />.
+        /// </returns>
+        private static bool PortNumberIsInCorrectRange(int portNumber)
+            => portNumber.IsInRangeWithLowerBound(1, 65535);
     }
 }
