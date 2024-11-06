237ed788e9e25e37dbc50cd91f30cb681acb5d76
Brian Hart <brian.hart@xylogix.net>
2024-11-06 16:33:38 -0700

    Create GetPathnameValidator.cs

---
+++ xyLOGIX.Validators.Factories/GetPathnameValidator.cs
@@ -0,0 +1,56 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Validators.Interfaces;
+
+namespace xyLOGIX.Validators.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Validators.Interfaces.IPathnameValidator" /> interface.
+    /// </summary>
+    public static class GetPathnameValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:xyLOGIX.Validators.Factories.GetPathnameValidator" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetPathnameValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IPathnameValidator" /> interface,
+        /// and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IPathnameValidator" /> interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        public static IPathnameValidator SoleInstance()
+        {
+            IPathnameValidator result;
+
+            try
+            {
+                result = PathnameValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
