dd74ba9cf11028ef4800859ccb36e9f244cd9dac
Brian Hart <brian.hart@xylogix.net>
2024-11-06 16:34:33 -0700

    Create PathnameValidatorTests.cs

---
+++ xyLOGIX.Validators.Tests/PathnameValidatorTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace xyLOGIX.Validators.Tests
+{
+    /// <summary>
+    /// Provides unit tests for the methods, properties, and events of the
+    /// <see cref="T:xyLOGIX.Validators.PathnameValidator" /> class.
+    /// </summary>
+    [TestFixture]
+    public class PathnameValidatorTests
+    {
+        // TODO: Add test fixture implementation here
+    }
+}
