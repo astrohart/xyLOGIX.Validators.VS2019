5e88d41951a45c9d1ccaa3f7e3dd1cb79db30d8f
Brian Hart <brian.hart@xylogix.net>
2025-02-02 15:56:25 -0700

    Add MaxPathLength constants and update project file
    * Added `xyLOGIX.Validators.Constants` namespace with `MaxPathLength` class.
      - Defined `Legacy` constant with value 260.
      - Defined `NTFS` constant with value 32767.
    * Modified `<NuGetPackageImportStamp>` element in `xyLOGIX.Validators.Constants.csproj` to include a newline.
    * Added an empty `<ItemGroup>` element to `xyLOGIX.Validators.Constants.csproj`.
    * Included `MaxPathLength.cs` in the `<Compile>` item group in `xyLOGIX.Validators.Constants.csproj`.

---
+++ xyLOGIX.Validators.Constants/MaxPathLength.cs
@@ -0,0 +1,21 @@
+﻿namespace xyLOGIX.Validators.Constants
+{
+    /// <summary>
+    /// Value(s) for the length(s) of pathname(s) that are considered to be the maximum
+    /// length(s).
+    /// </summary>
+    public static class MaxPathLength
+    {
+        /// <summary>
+        /// Maximum length of a pathname in the Windows operating system according to the
+        /// legacy value of <c>MAX_PATH</c>.
+        /// </summary>
+        public const int Legacy = 260;
+
+        /// <summary>
+        /// Maximum length of a pathname in the Windows operating system according to the
+        /// NTFS value.
+        /// </summary>
+        public const int NTFS = 32767;
+    }
+}
