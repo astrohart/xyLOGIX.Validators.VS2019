c7bd4305e335796ef927254573628ab87f2cd1e3
Brian Hart <brian.hart@xylogix.net>
2025-02-02 15:55:51 -0700

    Add Is class to check Long Path Support
    * Added new class `Is` in `xyLOGIX.Validators.Actions` namespace.
    * Implemented static method `LongPathSupportEnabled` to check if Long Path Support is enabled by querying the Windows Registry.
      - If the registry check fails, it assumes the legacy behavior and returns `false`.
    * Decorated the static constructor of `Is` class with `[Log(AttributeExclude = true)]` to simplify logging output.
    * Added `Microsoft.Win32` and `PostSharp.Patterns.Diagnostics` namespaces to `Is.cs`.
    * Included `Is.cs` file in the `xyLOGIX.Validators.Actions.csproj` project file.

---
+++ xyLOGIX.Validators.Actions/Is.cs
@@ -0,0 +1,54 @@
+using Microsoft.Win32;
+using PostSharp.Patterns.Diagnostics;
+
+namespace xyLOGIX.Validators.Actions
+{
+    /// <summary>
+    /// Exposes static method(s) that return <c>bool</c> values indicating whether a
+    /// given data element is of a certain type or has a certain value
+    /// </summary>
+    public static class Is
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Validators.Actions.Is" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static Is() { }
+
+        /// <summary>Determines whether the Long Path Support is enabled in the system.</summary>
+        /// <returns>
+        /// <see langword="true" /> if Long Path Support is enabled; otherwise,
+        /// <see langword="false" />.
+        /// </returns>
+        /// <remarks>
+        /// This method checks the Windows Registry to determine if the Long Path
+        /// Support is enabled. If the registry check fails, it assumes the legacy behavior
+        /// and returns <see langword="false" />.
+        /// </remarks>
+        public static bool LongPathSupportEnabled()
+        {
+            try
+            {
+                using (var key = Registry.LocalMachine.OpenSubKey(
+                           @"SYSTEM\CurrentControlSet\Control\FileSystem"
+                       ))
+                {
+                    return key?.GetValue("LongPathsEnabled")
+                              ?.Equals(1) == true;
+                }
+            }
+            catch
+            {
+                return false; // Assume legacy behavior if registry check fails
+            }
+        }
+    }
+}
