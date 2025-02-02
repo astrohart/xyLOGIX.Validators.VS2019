858b3667a68428e75fbea7b276cbe910d4fb012f
Brian Hart <brian.hart@xylogix.net>
2025-02-02 15:07:07 -0700

    Update IPathnameValidator.cs
    * Added `IsValidFilePath` method to `IPathnameValidator` interface.
    * Updated namespace declaration to include `PostSharp.Patterns.Diagnostics`.
    * Included detailed XML documentation for `IsValidFilePath` method.
    * Marked `pathname` parameter with `[NotLogged]` attribute.

--- xyLOGIX.Validators.Interfaces/IPathnameValidator.cs
+++ xyLOGIX.Validators.Interfaces/IPathnameValidator.cs
@@ -1,4 +1,6 @@
-﻿namespace xyLOGIX.Validators.Interfaces
+﻿using PostSharp.Patterns.Diagnostics;
+
+namespace xyLOGIX.Validators.Interfaces
 {
     /// <summary>
     /// Defines the publicly-exposed events, methods and properties of object(s) that
@@ -7,6 +9,28 @@
     /// </summary>
     public interface IPathnameValidator : IDataValidator
     {
+        /// <summary>
+        /// Validates that the specified file <paramref name="pathname" /> is of a valid
+        /// format on the Windows operating system.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname that is to be examined.
+        /// </param>
+        /// <remarks>
+        /// Disallows trailing backslashes.
+        /// <para />
+        /// If the value of the <paramref name="pathname" /> parameter is the
+        /// <see langword="null" />, blank, or <see cref="F:System.String.Empty" />
+        /// <see cref="T:System.String" />, then this method returns
+        /// <see langword="false" />.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="pathname" /> is a
+        /// properly-formatted file pathname; <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValidFilePath([NotLogged] string pathname);
+
         /// <summary>
         /// Validates that the specified folder <paramref name="pathname" /> is of
         /// a valid format on the Windows operating system.
