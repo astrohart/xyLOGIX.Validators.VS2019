628c6cf6b5a16c5b6c5cb09ed31f98aae8992075
Brian Hart <brian.hart@xylogix.net>
2025-03-15 09:52:00 -0600

    Update ValidationFailedEventArgs.cs
    * Added a static constructor with [Log(AttributeExclude = true)].
    - This constructor initializes static data or performs one-time actions for the class.
    
    * Updated the constructor of ValidationFailedEventArgs.
    - The error message parameter now includes the [Log(AttributeExclude = true)] attribute to prevent logging.
    
    * Added the [ExplicitlySynchronized] attribute to the class definition.
    
    * Corrected the order of using directives to place System.Diagnostics after PostSharp.Patterns.Diagnostics and PostSharp.Patterns.Threading.

--- xyLOGIX.Validators.Events/ValidationFailedEventArgs.cs
+++ xyLOGIX.Validators.Events/ValidationFailedEventArgs.cs
@@ -1,7 +1,7 @@
 ﻿using PostSharp.Patterns.Diagnostics;
-using System.Diagnostics;
 using PostSharp.Patterns.Threading;
 using System;
+using System.Diagnostics;
 
 namespace xyLOGIX.Validators.Events
 {
@@ -10,9 +10,24 @@ namespace xyLOGIX.Validators.Events
     /// Such events normally are raised when we failed to validate an
     /// object's properties' values according to a given set of rules.
     /// </remarks>
-[ExplicitlySynchronized]
+    [ExplicitlySynchronized]
     public class ValidationFailedEventArgs : EventArgs
     {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static ValidationFailedEventArgs() { }
+
         /// <summary>
         /// Constructs a new instance of
         /// <see cref="T:xyLOGIX.Validators.Events.ValidationFailedEventArgs" /> and
@@ -22,6 +37,7 @@ namespace xyLOGIX.Validators.Events
         /// (Required.) String containing the error message to
         /// be generated.
         /// </param>
+        [Log(AttributeExclude = true)]
         public ValidationFailedEventArgs([NotLogged] string errorMessage)
             => ErrorMessage = errorMessage;
 
