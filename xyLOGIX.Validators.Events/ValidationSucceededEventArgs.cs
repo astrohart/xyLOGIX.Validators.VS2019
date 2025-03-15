a8019c08e90e09e661dcfccb0cadeb82269124b1
Brian Hart <brian.hart@xylogix.net>
2025-03-15 10:03:49 -0600

    Update ValidationSucceededEventArgs.cs
    * Introduced `PostSharp.Patterns.Diagnostics` and
      `PostSharp.Patterns.Threading` namespaces for logging
      and threading synchronization.
    * Marked `ValidationSucceededEventArgs` class with
      `[ExplicitlySynchronized]` for thread safety.
    * Added a static constructor to the class, excluded
      from logging with `[Log(AttributeExclude = true)]`.
    * Added a public constructor, also excluded from
      logging with `[Log(AttributeExclude = true)]`.
    * Enhanced class with thread safety and logging
      capabilities, with clear documentation via XML comments.

--- xyLOGIX.Validators.Events/ValidationSucceededEventArgs.cs
+++ xyLOGIX.Validators.Events/ValidationSucceededEventArgs.cs
@@ -1,7 +1,31 @@
-﻿using System;
+﻿using PostSharp.Patterns.Diagnostics;
+using PostSharp.Patterns.Threading;
+using System;
 
 namespace xyLOGIX.Validators.Events
 {
     /// <summary> Provides information for ValidationSucceeded event handlers. </summary>
-    public class ValidationSucceededEventArgs : EventArgs { }
+    [ExplicitlySynchronized]
+    public class ValidationSucceededEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed once only
+        /// for the <see cref="T:xyLOGIX.Validators.Events.ValidationSucceededEventArgs" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance being
+        /// created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static ValidationSucceededEventArgs() { }
+
+        /// <summary>
+        /// Creates a new instance of
+        /// <see cref="T:xyLOGIX.Validators.Events.ValidationSucceededEventArgs" /> and
+        /// returns a reference to it.
+        /// </summary>
+        [Log(AttributeExclude = true)]
+        public ValidationSucceededEventArgs() { }
+    }
 }
