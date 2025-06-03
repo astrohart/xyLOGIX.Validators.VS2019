848efab61899df7932418f6e26c9dab9cf3d3af8
Brian Hart <brian.hart@xylogix.net>
2025-06-03 10:54:29 -0600

    Update Determine.cs
    - Add `using System.Diagnostics;` directive.
    - Remove static constructor from `Determine` class.
    - Apply `[Log(AttributeExclude = true)]` to the class.
    - Add `[DebuggerStepThrough]` attribute to `TheFileSystemTypeInUse` method for improved debugging.

--- xyLOGIX.Validators.Actions/Determine.cs
+++ xyLOGIX.Validators.Actions/Determine.cs
@@ -1,5 +1,6 @@
 using PostSharp.Patterns.Diagnostics;
 using System;
+using System.Diagnostics;
 using xyLOGIX.Core.Debug;
 using xyLOGIX.Validators.Constants;
 
@@ -9,22 +10,9 @@ namespace xyLOGIX.Validators.Actions
     /// Exposes static method(s) that are used to determine whether a given value is
     /// valid or not, or to make other determination(s) from various data value(s).
     /// </summary>
+    [Log(AttributeExclude = true)]
     public static class Determine
     {
-        /// <summary>
-        /// Initializes static data or performs actions that need to be performed once only
-        /// for the <see cref="T:xyLOGIX.Validators.Actions.Determine" /> class.
-        /// </summary>
-        /// <remarks>
-        /// This constructor is called automatically prior to the first instance being
-        /// created or before any static members are referenced.
-        /// <para />
-        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
-        /// attribute in order to simplify the logging output.
-        /// </remarks>
-        [Log(AttributeExclude = true)]
-        static Determine() { }
-
         /// <summary>
         /// Attempts to determine the type of file system that is currently in use.
         /// </summary>
@@ -42,7 +30,7 @@ namespace xyLOGIX.Validators.Actions
         /// the method returns
         /// <see cref="F:xyLOGIX.Validators.Constants.FileSystemType.Unknown" />.
         /// </returns>
-        [Log(AttributeExclude = true)]
+        [DebuggerStepThrough]
         public static FileSystemType TheFileSystemTypeInUse()
         {
             var result = FileSystemType.Unknown;
