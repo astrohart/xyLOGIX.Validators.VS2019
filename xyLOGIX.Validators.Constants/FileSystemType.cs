b5e1ff978a3d5cde700ec251184f7fb32b5c0f79
Brian Hart <brian.hart@xylogix.net>
2025-06-03 10:46:07 -0600

    Create FileSystemType.cs
    - Added new enumeration `FileSystemType` with values:
      - `Legacy`: Represents legacy file systems like FAT32.
      - `NTFS`: Represents the NTFS file system.
      - `Unknown`: Indicates an unknown file system type.
    
    - Included `FileSystemType.cs` in the project file
      `xyLOGIX.Validators.Constants.csproj` for compilation.

---
+++ xyLOGIX.Validators.Constants/FileSystemType.cs
@@ -0,0 +1,25 @@
+﻿namespace xyLOGIX.Validators.Constants
+{
+    /// <summary>
+    /// Value(s) that indicate the type of file system that is present (e.g., NTFS,
+    /// FAT32, etc.).
+    /// </summary>
+    public enum FileSystemType
+    {
+        /// <summary>
+        /// A file system that is formatted using the <c>FAT32</c> or other legacy file
+        /// system format.
+        /// </summary>
+        Legacy,
+
+        /// <summary>
+        /// A file system that is formatted using the <c>NTFS</c> file system format.
+        /// </summary>
+        NTFS,
+
+        /// <summary>
+        /// Unknown file system type.
+        /// </summary>
+        Unknown = -1
+    }
+}
