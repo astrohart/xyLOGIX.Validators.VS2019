acd626fb38f8bc800d309135040b855ae10c6f39
Brian Hart <brian.hart@xylogix.net>
2025-07-27 14:05:39 -0600

    Create ISolutionPathnameValidator.cs
    - Introduce new interface `ISolutionPathnameValidator`
      - Defines method `IsValid` for validating Visual Studio Solution pathnames
      - Includes XML documentation for clarity
    - Add `ISolutionPathnameValidator.cs` to project file
    - Remove unnecessary line from `xyLOGIX.Validators.Interfaces.csproj`

---
+++ xyLOGIX.Validators.Interfaces/ISolutionPathnameValidator.cs
@@ -0,0 +1,36 @@
+﻿using PostSharp.Patterns.Diagnostics;
+
+namespace xyLOGIX.Validators.Interfaces
+{
+    /// <summary>
+    /// Defines the publicly-exposed events, methods and properties of object(s) that
+    /// validate the format of <see cref="T:System.String" /> values that purport to
+    /// contain pathnames..
+    /// </summary>
+    public interface ISolutionPathnameValidator : IDataValidator
+    {
+        /// <summary>
+        /// Validates that the specified Visual Studio Solution (<c>*.sln</c>)
+        /// <paramref name="pathname" /> is of a valid format on the Windows operating
+        /// system, and that it is a valid pathname of a Visual Studio Solution (
+        /// <c>*.sln</c>) file.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname that is to be examined.
+        /// </param>
+        /// <remarks>
+        /// Disallows trailing backslashes.
+        /// <para />
+        /// If the value of the <paramref name="pathname" /> parameter is the
+        /// <see langword="null" />, blank, or <see cref="F:System.String.Empty" />
+        /// <see cref="T:System.String" />, then this method returns
+        /// <see langword="false" />.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="pathname" /> is a
+        /// properly-formatted file pathname; <see langword="false" /> otherwise.
+        /// </returns>
+        bool IsValid([NotLogged] string pathname);
+    }
+}
