45162256a29354138af29717122d1f4926d28d77
Brian Hart <brian.hart@xylogix.net>
2025-08-02 16:36:07 -0600

    Create GetProjectPathnameValidator.cs
    - Introduce a new static class `GetProjectPathnameValidator`.
    - Provide access to a singleton instance of `IProjectPathnameValidator`.
    - Implement a static constructor for initialization.
    - Add `SoleInstance` method to retrieve the singleton instance.
    - Handle exceptions and log them using `DebugUtils`.
    - Include `GetProjectPathnameValidator.cs` in the project file.

---
+++ xyLOGIX.Validators.Factories/GetProjectPathnameValidator.cs
@@ -0,0 +1,60 @@
+﻿using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Diagnostics;
+using xyLOGIX.Core.Debug;
+using xyLOGIX.Validators.Interfaces;
+
+namespace xyLOGIX.Validators.Factories
+{
+    /// <summary>
+    /// Provides access to the one and only instance of the object that implements the
+    /// <see cref="T:xyLOGIX.Validators.Interfaces.IProjectPathnameValidator" />
+    /// interface.
+    /// </summary>
+    public static class GetProjectPathnameValidator
+    {
+        /// <summary>
+        /// Initializes static data or performs actions that need to be performed
+        /// once only for the
+        /// <see cref="T:xyLOGIX.Validators.Factories.GetProjectPathnameValidator" />
+        /// class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any static members are referenced.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static GetProjectPathnameValidator() { }
+
+        /// <summary>
+        /// Obtains access to the sole instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IProjectPathnameValidator" />
+        /// interface, and returns a reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to the one, and only, instance of the object that implements the
+        /// <see cref="T:xyLOGIX.Validators.Interfaces.IProjectPathnameValidator" />
+        /// interface.
+        /// </returns>
+        [DebuggerStepThrough]
+        [return: NotLogged]
+        public static IProjectPathnameValidator SoleInstance()
+        {
+            IProjectPathnameValidator result;
+
+            try
+            {
+                result = ProjectPathnameValidator.Instance;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                DebugUtils.LogException(ex);
+
+                result = default;
+            }
+
+            return result;
+        }
+    }
+}
