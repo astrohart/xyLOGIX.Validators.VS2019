734438886ff79cf60d053144e89ea5f2c9e4a1f5
Brian Hart <brian.hart@xylogix.net>
2025-08-22 16:37:46 -0600

    Remove Synchronized attribute from IDataValidator
    - Removed the `[Synchronized]` attribute from the
      `IDataValidator` interface.
    - This change simplifies the interface definition
      in the `xyLOGIX.Validators.Interfaces` namespace.

--- xyLOGIX.Validators.Interfaces/IDataValidator.cs
+++ xyLOGIX.Validators.Interfaces/IDataValidator.cs
@@ -6,7 +6,6 @@ namespace xyLOGIX.Validators.Interfaces
     /// Defines the publicly-exposed methods and properties that are in common to all
     /// data validators.
     /// </summary>
-    [Synchronized]
     public interface IDataValidator
     {
         // TODO: Add events, methods, and properties to your interface here.
