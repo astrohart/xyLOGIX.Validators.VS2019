1ffc0629982721c6915f305f7e2b2390ab011d9a
Brian Hart <brian.hart@xylogix.net>
2025-09-07 14:05:44 -0600

    Create ValidProjectFilenameExtensions.cs
    - Added a new class `ValidProjectFilenameExtensions`
      - Defines a static readonly array of valid project filename extensions for Visual Studio IDE:
        - `*.csproj`
        - `*.vbproj`
        - `*.fsproj`
      - Includes new using directives for:
        - `Alphaleonis.Win32.Filesystem`
        - `PostSharp.Patterns.Diagnostics`
        - `System.Linq`
    
    - Updated `xyLOGIX.Validators.Constants.csproj`
      - Added reference to `ValidProjectFilenameExtensions.cs` in compilation items
      - Modified closing tag to ensure proper XML structure

---
+++ xyLOGIX.Validators.Constants/ValidProjectFilenameExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Alphaleonis.Win32.Filesystem;
+using PostSharp.Patterns.Diagnostics;
+using System;
+using System.Linq;
+
+namespace xyLOGIX.Validators.Constants
+{
+    /// <summary>
+    /// Value(s) for the filename extension(s) of the project (<c>*.*proj</c>) file(s)
+    /// that are supported by modern version(s) of the Visual Studio IDE.
+    /// </summary>
+    public class ValidProjectFilenameExtensions
+    {
+        /// <summary>
+        /// Gets the list of valid project filename extensions.
+        /// </summary>
+        public static readonly string[] Extensions =
+        {
+            "*.csproj", "*.vbproj", "*.fsproj"
+        };
+    }
+}
