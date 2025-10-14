8b487b54241fe0d96fb024dbefeb2852e6c7bc26
Brian Hart <brian.hart@xylogix.net>
2025-10-14 13:50:48 -0600

    Update IPathnameValidator.cs
    - Adds a new method `IsValidFilePath` to the `IPathnameValidator` interface.
    - Validates that a specified file pathname is in a valid format for Windows.
    - Includes detailed documentation for the new method:
      - Describes the method's purpose and parameters.
      - Specifies return values and validation rules.
      - Disallows trailing backslashes and handles null or empty strings.

--- xyLOGIX.Validators.Interfaces/IPathnameValidator.cs
+++ xyLOGIX.Validators.Interfaces/IPathnameValidator.cs
@@ -29,6 +29,28 @@ namespace xyLOGIX.Validators.Interfaces
         /// <see langword="true" /> if the specified <paramref name="pathname" /> is a
         /// properly-formatted file pathname; <see langword="false" /> otherwise.
         /// </returns>
+        bool IsValidFilePath([NotLogged] string pathname);
+
+        /// <summary>
+        /// Validates that the specified file <paramref name="pathname" /> is of a valid
+        /// format on the Windows operating system.
+        /// </summary>
+        /// <param name="pathname">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname that is to be examined.
+        /// </param>
+        /// <remarks>
+        /// Disallows trailing backslashes.
+        /// <para />
+        /// If the value of the <paramref name="pathname" /> parameter is the
+        /// <see langword="null" />, blank, or <see cref="F:System.String.Empty" />
+        /// <see cref="T:System.String" />, then this method returns
+        /// <see langword="false" />.
+        /// </remarks>
+        /// <returns>
+        /// <see langword="true" /> if the specified <paramref name="pathname" /> is a
+        /// properly-formatted file pathname; <see langword="false" /> otherwise.
+        /// </returns>
         bool IsValidFilePathSilent([NotLogged] string pathname);
 
         /// <summary>
