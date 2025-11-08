711fc4c664349b37dc47b57d7308fcfca45c9153
Brian Hart <brian.hart@xylogix.net>
2025-11-08 13:47:33 -0700

    Update AssemblyInfo.cs
    - Removed all reference(s) to the `xyLOGIX.Files.Action` module, as it is being retired.  Also, reformatted the source file(s) in accordance with rules that are defined in CodeMaid and ReSharper.

--- xyLOGIX.Validators.Tests/Properties/AssemblyInfo.cs
+++ xyLOGIX.Validators.Tests/Properties/AssemblyInfo.cs
@@ -5,16 +5,21 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("xyLOGIX.Validators.Tests")]
-[assembly: AssemblyDescription("Provides unit tests for various validator objects within this framework.")]
+[assembly:
+    AssemblyDescription(
+        "Provides unit tests for various validator objects within this framework."
+    )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xyLOGIX, LLC")]
 [assembly: AssemblyProduct("Unit Tests for Data Validation")]
 [assembly:
     AssemblyCopyright(
-        "Copyright © 2020-25 by xyLOGIX, LLC.  All rights reserved.")]
+        "Copyright © 2020-25 by xyLOGIX, LLC.  All rights reserved."
+    )]
 [assembly:
     AssemblyTrademark(
-        "This software is a component of xyLOGIX Unit Tests for Data Validation.  Unit Tests for Data Validation is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+        "This software is a component of xyLOGIX Unit Tests for Data Validation.  Unit Tests for Data Validation is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties."
+    )]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
