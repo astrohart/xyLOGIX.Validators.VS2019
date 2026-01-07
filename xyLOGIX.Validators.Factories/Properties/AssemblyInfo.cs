b2b31011be61a837c2dd1429bbcc0d8c4996ea57
Brian Hart <brian.hart@xylogix.net>
2026-01-07 11:21:38 -0700

    Update AssemblyInfo.cs
    - Updated the copyright year to extend to 2026.

--- xyLOGIX.Validators.Factories/Properties/AssemblyInfo.cs
+++ xyLOGIX.Validators.Factories/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
     AssemblyProduct("xyLOGIX Support Library for xyLOGIX.Validators.Factories")]
 [assembly:
     AssemblyCopyright(
-        "Copyright © 2021-25 by xyLOGIX, LLC.  All rights reserved."
+        "Copyright © 2021-26 by xyLOGIX, LLC.  All rights reserved."
     )]
 [assembly:
     AssemblyTrademark(
